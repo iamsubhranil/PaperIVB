c6315151295e8f910ddc5fd707f6e21e977e8bce
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2018-04-30 19:39:20 +0530

    common : prevent redefining sample size

--- common.h
+++ common.h
@@ -13,5 +13,8 @@ typedef int64_t midx;
 #define PRIidx PRId64
 #define SCNidx SCNd64
 
+#ifndef SORT_TEST_ITEM_COUNT // Don't redefine sample count if it is specified at cmd
 #define SORT_TEST_ITEM_COUNT 10000 // denotes the number of samples in each sort test
+#endif
+
 //#define QUICK_SORT_VISUAL // toggles the visualization of the quick sort algorithm
