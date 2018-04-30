a55885fda88c0e00a941ca9de2a8969af2d34662
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2018-04-30 22:10:12 +0530

    common : added short type

--- common.h
+++ common.h
@@ -9,6 +9,11 @@ typedef int64_t mint;
 #define SCNint SCNd64
 #define MINT_MAX INT64_MAX
 
+typedef int8_t mints; // short integer
+#define PRIints PRId8
+#define SCNints SCNd8
+#define MINTS_MAX INT8_MAX
+
 typedef int64_t midx;
 #define PRIidx PRId64
 #define SCNidx SCNd64
