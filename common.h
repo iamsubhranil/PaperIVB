d196ac2bf1ff198527273d7cd7ecf4ac5793f9a5
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2018-05-06 17:37:58 +0530

    common : moved all sort related macros to sort.h

--- common.h
+++ common.h
@@ -17,24 +17,3 @@ typedef int8_t mints; // short integer
 typedef int64_t midx;
 #define PRIidx PRId64
 #define SCNidx SCNd64
-
-#ifndef SORT_TEST_ITEM_COUNT // Don't redefine sample count if it is specified at cmd
-#define SORT_TEST_ITEM_COUNT 10000 // denotes the number of samples in each sort test
-#endif
-
-//#define QUICK_SORT_VISUAL // toggles the visualization of the quick sort algorithm
-
-#ifdef SORT_ENABLE_TEST_ALL
-#define SORT_ENABLE_TEST_BUBBLE
-#define SORT_ENABLE_TEST_BUBBLE_REC
-#define SORT_ENABLE_TEST_BUBBLE_ADV
-#define SORT_ENABLE_TEST_SELECTION
-#define SORT_ENABLE_TEST_SELECTION_REC
-#define SORT_ENABLE_TEST_INSERTION
-#define SORT_ENABLE_TEST_INSERTION_REC
-#define SORT_ENABLE_TEST_QUICK
-#define SORT_ENABLE_TEST_QUICK_NONREC
-#define SORT_ENABLE_TEST_HEAP
-#define SORT_ENABLE_TEST_MERGE
-#define SORT_ENABLE_TEST_MERGE_NONREC
-#endif
--- sort.h
+++ sort.h
@@ -25,3 +25,24 @@ ADD_METHOD(bubble_sort_rec, void)
 ADD_METHOD(bubble_sort_adv, void)
 
 void test_sort();
+
+#ifndef SORT_TEST_ITEM_COUNT // Don't redefine sample count if it is specified at cmd
+#define SORT_TEST_ITEM_COUNT 10000 // denotes the number of samples in each sort test
+#endif
+
+//#define QUICK_SORT_VISUAL // toggles the visualization of the quick sort algorithm
+
+#ifdef SORT_ENABLE_TEST_ALL
+#define SORT_ENABLE_TEST_BUBBLE
+#define SORT_ENABLE_TEST_BUBBLE_REC
+#define SORT_ENABLE_TEST_BUBBLE_ADV
+#define SORT_ENABLE_TEST_SELECTION
+#define SORT_ENABLE_TEST_SELECTION_REC
+#define SORT_ENABLE_TEST_INSERTION
+#define SORT_ENABLE_TEST_INSERTION_REC
+#define SORT_ENABLE_TEST_QUICK
+#define SORT_ENABLE_TEST_QUICK_NONREC
+#define SORT_ENABLE_TEST_HEAP
+#define SORT_ENABLE_TEST_MERGE
+#define SORT_ENABLE_TEST_MERGE_NONREC
+#endif
