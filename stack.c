27775791f6191fa7f3d015be96796a6cd3b48bf1
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2018-05-06 14:59:44 +0530

    Revert "Revert "stack : generified API for providing an unified interface of multiple types""
    This reverts commit c6413c23aea48dd2e661469d9b7946340387bd91.

--- stack.c
+++ stack.c
@@ -2,59 +2,10 @@
 
 #include "stack.h"
 #include "test.h"
+#include "arr.h"
 
-Stack_Intr* stack_new(midx count, mints grow){
-    Stack_Intr *stack = (Stack_Intr*)malloc(sizeof(Stack_Intr));
-    stack->count = count > 0 ? count : 0;
-    stack->values = count == 0 ? NULL : (mint *)malloc(sizeof(mint) * count);
-    stack->top = stack->values;
-    stack->grow = grow ? 1 : 0;
-    stack->status = 0;
-    return stack;
-}
-
-void stack_push_fast(Stack_Intr *stack, mint value){
-    *(stack->top) = value;
-    stack->top++;
-}
-
-void stack_push(Stack_Intr *stack, mint value){
-    if(stack->top == NULL || stack->top == stack->values + stack->count - 1){
-        if(!stack->grow){
-            stack->status |= 1;
-            return;
-        }
-        mint oldCount = stack->count;
-        (stack->count == 0) ? (stack->count = 1) : (stack->count *= 2);
-        stack->values = (mint *)realloc(stack->values, sizeof(mint) * stack->count);
-        stack->top = stack->values + (oldCount == 0 ? 0 :oldCount - 1);
-    }
-    *(stack->top) = value;
-    stack->top++;
-    if(stack->status & 2)
-        stack->status = 0;
-}
-
-mint stack_pop_fast(Stack_Intr *stack){
-    return *(--stack->top);
-}
-
-mint stack_pop(Stack_Intr *stack){
-    if(stack->top == stack->values){
-        stack->status |= 2;
-        return 0;
-    }
-    if(stack->status & 1)
-        stack->status = 0;
-    return *(--stack->top);
-}
-
-void stack_free(Stack_Intr *stack){
-    free(stack->values);
-    stack->count = 0;
-    stack->top = stack->values = NULL;
-    free(stack);
-}
+// Type agnostic API
+// =================
 
 mint stack_is_empty(Stack_Intr *stack){
     return stack->top == stack->values;
@@ -68,97 +19,182 @@ mint stack_is_underflow(Stack_Intr *stack){
     return stack->status & 2;
 }
 
-mint stack_size(Stack_Intr *stack){
-    return stack->top - stack->values;
+// Type sensitive API
+// ==================
+
+#define stk_api(name_new, name_push, name_pushf, name_pop, name_popf, name_free, \
+        name_size, values, top, type, type_rand) \
+Stack_Intr* stack_##name_new(midx count, mints grow){ \
+    Stack_Intr *stack = (Stack_Intr*)malloc(sizeof(Stack_Intr)); \
+    stack->count = count > 0 ? count : 0; \
+    stack->values = count == 0 ? NULL : (type *)malloc(sizeof(type) * count); \
+    stack->top = stack->values; \
+    stack->grow = grow ? 1 : 0; \
+    stack->status = 0; \
+    return stack; \
+} \
+ \
+void stack_##name_pushf(Stack_Intr *stack, type value){ \
+    *(stack->top) = value; \
+    stack->top++; \
+} \
+\
+void stack_##name_push(Stack_Intr *stack, type value){ \
+    if(stack->top == NULL || stack->top == stack->values + stack->count - 1){ \
+        if(!stack->grow){ \
+            stack->status |= 1; \
+            return; \
+        } \
+        mint oldCount = stack->count; \
+        (stack->count == 0) ? (stack->count = 1) : (stack->count *= 2); \
+        stack->values = (type *)realloc(stack->values, sizeof(type) * stack->count); \
+        stack->top = stack->values + (oldCount == 0 ? 0 :oldCount - 1); \
+    } \
+    *(stack->top) = value; \
+    stack->top++; \
+    if(stack->status & 2) \
+        stack->status = 0; \
+} \
+ \
+type stack_##name_popf(Stack_Intr *stack){ \
+    return *(--stack->top); \
+} \
+ \
+type stack_##name_pop(Stack_Intr *stack){ \
+    if(stack->top == stack->values){ \
+        stack->status |= 2; \
+        return 0; \
+    } \
+    if(stack->status & 1) \
+        stack->status = 0; \
+    return *(--stack->top); \
+} \
+ \
+void stack_##name_free(Stack_Intr *stack){ \
+    free(stack->values); \
+    stack->count = 0; \
+    stack->top = stack->values = NULL; \
+    free(stack); \
+} \
+\
+mint stack_##name_size(Stack_Intr *stack){ \
+    return stack->top - stack->values; \
+} \
+ \
+static mint test_stack_##name_new(midx count){ \
+    Stack_Intr* s = stack_##name_new(count, 0); \
+    if(s->count == count && s->values != NULL){ \
+        stack_##name_free(s); \
+        return 1; \
+    } \
+    return 0; \
+} \
+\
+static mint test_stack_##name_push(Stack_Intr *stack){ \
+    type *bak = stack->top; \
+    type randel = type_rand(); \
+    stack_##name_push(stack, randel); \
+    if(*(stack->top - 1) != randel) \
+        return 0; \
+    randel = type_rand(); \
+    stack_##name_push(stack, randel); \
+    if(*(stack->top - 1) != randel) \
+        return 0; \
+    stack->top = bak; \
+    return 1; \
+} \
+\
+static mint test_stack_grow_##name_push(Stack_Intr *stack){ \
+    for(mint i = 0;i < 10000;i++) \
+        stack_##name_push(stack, type_rand()); \
+    mint ret = 0; \
+    if(stack->top - stack->values == 10000){ \
+        ret = 1; \
+    } \
+    stack->top -= 10000; \
+    return ret; \
+} \
+\
+static mint test_stack_##name_pop(Stack_Intr *stack){ \
+    for(mint i = 0;i < 10000;i++){ \
+        type randel = type_rand(); \
+        stack_##name_push(stack, randel); \
+        if(stack_##name_pop(stack) != randel) \
+            return 0; \
+    } \
+    return 1; \
+} \
+\
+static mint test_stack_##name_size(Stack_Intr *stack){ \
+    stack->top = stack->values; \
+    mint size = 4389; \
+    for(mint i = 0;i < size;i++) \
+        stack_##name_push(stack, type_rand()); \
+    if(stack_##name_size(stack) != size) \
+        return 0; \
+    stack->top = stack->values; \
+    return 1; \
+} \
+ \
+static mint test_stack_empty_##name_pop(Stack_Intr *stack){ \
+    stack->top = stack->values; \
+    stack_##name_push(stack, type_rand()); \
+    if(stack_is_empty(stack)) \
+        return 0; \
+    stack_##name_pop(stack); \
+    return stack_is_empty(stack); \
+} \
+ \
+static mint test_stack_overflow_##name_push(Stack_Intr *stack){ \
+    for(mint i = 0;i < 12;i++) \
+        stack_##name_push(stack, type_rand()); \
+    return stack_is_overflow(stack); \
+} \
+ \
+static mint test_stack_underflow_##name_pop(Stack_Intr *stack){ \
+    for(mint i = 0;i < 12;i++) \
+        stack_##name_pop(stack); \
+    return stack_is_underflow(stack); \
+} \
+
+static mint rand_int(){
+    return random_at_most(rand());
 }
+
+stk_api(new, push, push_fast, pop, pop_fast, free,
+        size, values, top, mint, rand_int)
+
+static void* rand_void(){
+    return (void*)random();
+}
+
+stk_api(new_generic, push_generic, push_fast_generic, 
+        pop_generic, pop_fast_generic, free_generic,
+        size_generic, genericValues, genericTop, void*, rand_void)
+
+#undef stk_api
 
 // Tests
-// =========================
-
-static mint test_stack_new(midx count){
-    Stack_Intr* s = stack_new(count, 0);
-    if(s->count == count && s->values != NULL){
-        stack_free(s);
-        return 1;
-    }
-    return 0;
-}
-
-static mint test_stack_push(Stack_Intr *stack){
-    mint *bak = stack->top;
-    stack_push(stack, 392);
-    if(*(stack->top - 1) != 392)
-        return 0;
-    stack_push(stack, 8382);
-    if(*(stack->top - 1) != 8382)
-        return 0;
-    stack->top = bak;
-    return 1;
-}
-
-static mint test_stack_grow(Stack_Intr *stack){
-    for(mint i = 0;i < 10000;i++)
-        stack_push(stack, i);
-    mint ret = 0;
-    if(*(stack->top - 1) == 9999){
-        ret = 1;
-    }
-    stack->top -= 10000;
-    return ret;
-}
-
-static mint test_stack_pop(Stack_Intr *stack){
-    stack_push(stack, 372);
-    if(stack_pop(stack) != 372)
-        return 0;
-    stack_push(stack, -382);
-    if(stack_pop(stack) != -382)
-        return 0;
-    return 1;
-}
-
-static mint test_stack_empty(Stack_Intr *stack){
-    stack_push(stack, 3832);
-    if(stack_is_empty(stack))
-        return 0;
-    stack_pop(stack);
-    return stack_is_empty(stack);
-}
-
-static mint test_stack_overflow(Stack_Intr *stack){
-    for(mint i = 0;i < 12;i++)
-        stack_push(stack, 100);
-    return stack_is_overflow(stack);
-}
-
-static mint test_stack_underflow(Stack_Intr *stack){
-    for(mint i = 0;i < 12;i++)
-        stack_pop(stack);
-    return stack_is_underflow(stack);
-}
-
-static mint test_stack_size(Stack_Intr *stack){
-    stack->top = stack->values;
-    mint size = 4389;
-    for(mint i = 0;i < size;i++)
-        stack_push(stack, 3929);
-    if(stack_size(stack) != size)
-        return 0;
-    stack->top = stack->values;
-    return 1;
-}
+// =====
 
 void test_stack(){
-    TEST("Stack Creation", test_stack_new(10));
-    Stack stack = stack_new(10, 1);
-    Stack stack2 = stack_new(10, 0);
-    TEST("Stack Push", test_stack_push(stack));
-    TEST("Stack Overflow", test_stack_overflow(stack2));
-    TEST("Stack Grow", test_stack_grow(stack));
-    TEST("Stack Pop", test_stack_pop(stack));
-    TEST("Stack Underflow", test_stack_underflow(stack2));
-    TEST("Stack Size", test_stack_size(stack));
-    TEST("Stack Empty", test_stack_empty(stack));
-    stack_free(stack);
-    stack_free(stack2);
+#define stk_tst(name, name_new, name_push, name_pushf, name_pop, name_popf, name_size, name_free) \
+    TEST(#name " Stack Creation", test_stack_##name_new(10)); \
+    Stack stack_##name = stack_##name_new(10, 1); \
+    Stack stack2_##name = stack_##name_new(10, 0); \
+    TEST(#name " Stack Push", test_stack_##name_push(stack_##name)); \
+    TEST(#name " Stack Overflow", test_stack_overflow_##name_push(stack2_##name)); \
+    TEST(#name " Stack Grow", test_stack_grow_##name_push(stack_##name)); \
+    TEST(#name " Stack Pop", test_stack_##name_pop(stack_##name)); \
+    TEST(#name " Stack Underflow", test_stack_underflow_##name_pop(stack2_##name)); \
+    TEST(#name " Stack Size", test_stack_##name_size(stack_##name)); \
+    TEST(#name " Stack Empty", test_stack_empty_##name_pop(stack_##name)); \
+    stack_##name_free(stack_##name); \
+    stack_##name_free(stack2_##name);
+
+    stk_tst(Integer, new, push, push_fast, pop, pop_fast, size, free);
+    stk_tst(Generic, new_generic, push_generic, push_fast_generic, pop_generic, pop_fast_generic,
+            size_generic, free_generic);
+
+#undef stk_tst
 }
--- stack.h
+++ stack.h
@@ -3,8 +3,14 @@
 #include "common.h"
 
 typedef struct{
-    mint *values;
-    mint *top;
+    union{
+        mint *values;
+        void **genericValues;
+    };
+    union{
+        mint *top;
+        void **genericTop;
+    };
     midx count;
     mints grow;
     mints status; // 0-> Normal, 1-> Overflow, 2-> Underflow
@@ -18,8 +24,10 @@ typedef Stack_Intr* Stack;
 // occurs, nothing will be pushed to the stack
 // and the overflow flag will be set.
 Stack stack_new(midx count, mints grow);
+Stack stack_new_generic(midx count, mints grow);
 
 void stack_free(Stack stack);
+void stack_free_generic(Stack stack);
 
 // No overflow check and/or stack expansion,
 // use only when 200% sure about the state
@@ -27,17 +35,21 @@ void stack_free(Stack stack);
 // things may happen, heap corruption being
 // the minimum.
 void stack_push_fast(Stack stack, mint value);
+void stack_push_fast_generic(Stack stack, void *value);
 
 // Slower, safer push, with full overflow check
 // and stack expansion support.
 void stack_push(Stack stack, mint value);
+void stack_push_generic(Stack stack, void *value);
 
 // No underflow check, same cautions applicable
 // as stack_push_fast
 mint stack_pop_fast(Stack stack);
+void* stack_pop_fast_generic(Stack stack);
 
 // Slower, safer pop, with full underflow check
 mint stack_pop(Stack stack);
+void* stack_pop_generic(Stack stack);
 
 
 // Stack status query
@@ -45,5 +57,6 @@ mint stack_is_empty(Stack stack);
 mint stack_is_overflow(Stack stack);
 mint stack_is_underflow(Stack stack);
 mint stack_size(Stack stack);
+mint stack_size_generic(Stack stack);
 
 void test_stack();
