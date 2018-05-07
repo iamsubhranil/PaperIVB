4c72f2f3da190079c0401ea8a737c9e469744132
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2018-05-07 18:24:27 +0530

    display : updated the display API to be more verbose in presence of a DEBUG flag

--- display.h
+++ display.h
@@ -16,7 +16,21 @@ void pylw(const char *msg, ...);
 void pcyn(const char *msg, ...);
 void pmgn(const char *msg, ...);
 
+#ifdef DEBUG
+#define dbg(x, ...) dbg2( ANSI_FONT_BOLD "<%s:%d:%s> " ANSI_COLOR_RESET x, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+void dbg2(const char *msg, ...);
+
+#define err(x, ...) err2( ANSI_FONT_BOLD "<%s:%d:%s> " ANSI_COLOR_RESET x, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+void err2(const char *msg, ...);
+
+#define info(x, ...) info2( ANSI_FONT_BOLD "<%s:%d:%s> " ANSI_COLOR_RESET x, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+void info2(const char *msg, ...);
+
+#define warn(x, ...) warn2( ANSI_FONT_BOLD "<%s:%d:%s> " ANSI_COLOR_RESET x, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+void warn2(const char *msg, ...);
+#else
 void dbg(const char *msg, ...);
 void err(const char *msg, ...);
 void info(const char *msg, ...);
 void warn(const char *msg, ...);
+#endif
