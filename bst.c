93a495a6965dcb7861480f6720d16e080fb23a53
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2018-05-11 13:35:16 +0530

    bst : made core struct opaque

--- bst.c
+++ bst.c
@@ -5,17 +5,25 @@
 #include "test.h"
 #include "utils.h"
 #include "sort.h"
+#include "stack.h"
 
-static BinarySearchTree* bst_new_node(){
-    BinarySearchTree *node = (BinarySearchTree *)malloc(sizeof(BinarySearchTree));
+
+typedef struct BST{
+    i64 value;
+    struct BST *left;
+    struct BST *right;
+} BST;
+
+static BST* bst_new_node(){
+    BST *node = (BST *)malloc(sizeof(BST));
     node->left = NULL;
     node->right = NULL;
     node->value = 0;
     return node;
 }
 
-i64 bst_insert(BinarySearchTree *bst, i64 value){
-    BinarySearchTree *tmp = bst, *prev = bst;
+i64 bst_insert(BST *bst, i64 value){
+    BST *tmp = bst, *prev = bst;
     while(tmp != NULL){
         //if(value == tmp->value)
         //    return 0;
@@ -36,15 +44,15 @@ i64 bst_insert(BinarySearchTree *bst, i64 value){
     return 1;
 }
 
-BinarySearchTree *bst_create(i64 *arr, siz n){
-    BinarySearchTree *root = bst_new_node();
+BST *bst_create(i64 *arr, siz n){
+    BST *root = bst_new_node();
     root->value = arr[0];
     for(siz i = 1;i < n;i++)
         bst_insert(root, arr[i]);
     return root;
 }
 
-void bst_free(BinarySearchTree *bst){
+void bst_free(BST *bst){
     if(bst == NULL)
         return;
     bst_free(bst->left);
@@ -52,14 +60,14 @@ void bst_free(BinarySearchTree *bst){
     free(bst);
 }
 
-i64 bst_search(BinarySearchTree *bst, i64 value){
-    for(BinarySearchTree *tmp = bst;tmp != NULL;tmp = value > tmp->value ? tmp->right : tmp->left)
+i64 bst_search(BST *bst, i64 value){
+    for(BST *tmp = bst;tmp != NULL;tmp = value > tmp->value ? tmp->right : tmp->left)
         if(tmp->value == value)
             return 1;
     return 0;
 }
 
-void bst_preorder(BinarySearchTree *bst, bst_process process){
+void bst_preorder(BST *bst, bst_process process){
     if(bst == NULL)
         return;
     process(bst->value);
@@ -67,7 +75,7 @@ void bst_preorder(BinarySearchTree *bst, bst_process process){
     bst_preorder(bst->right, process);
 }
 
-void bst_inorder(BinarySearchTree *bst, bst_process process){
+void bst_inorder(BST *bst, bst_process process){
     if(bst == NULL)
         return;
     bst_inorder(bst->left, process);
@@ -75,7 +83,7 @@ void bst_inorder(BinarySearchTree *bst, bst_process process){
     bst_inorder(bst->right, process);
 }
 
-void bst_postorder(BinarySearchTree *bst, bst_process process){
+void bst_postorder(BST *bst, bst_process process){
     if(bst == NULL)
         return;
     bst_postorder(bst->left, process);
@@ -83,14 +91,14 @@ void bst_postorder(BinarySearchTree *bst, bst_process process){
     process(bst->value);
 }
 
-static BinarySearchTree *bst_test_gen;
+static BST *bst_test_gen;
 
 static i64 test_bst_create(siz n){
     tst_pause("Generating random elements");
     i64 *arr = arr_new(n);
     arr_fill_rand(arr, n, random_at_most(n), SAMPLE_CASE_AVERAGE);
     tst_resume("Creating BST");
-    BinarySearchTree *bst = bst_create(arr, n);
+    BST *bst = bst_create(arr, n);
     tst_resume("Cleaning up");
     arr_free(arr);
     if(!bst)
@@ -99,7 +107,7 @@ static i64 test_bst_create(siz n){
     return 1;
 }
 
-static i64 test_bst_insert(BinarySearchTree *bst, i64 value){
+static i64 test_bst_insert(BST *bst, i64 value){
     bst_insert(bst, value);
     if(!bst_search(bst, value))
         return 0;
@@ -127,7 +135,7 @@ static i64 test_bst_inorder(){
     bst_test_array = arr_new(BST_TEST_ITEM_COUNT);
     arr_fill_rand(bst_test_array, BST_TEST_ITEM_COUNT, 898213, SAMPLE_CASE_AVERAGE);
     
-    BinarySearchTree  *bst = bst_create(bst_test_array, BST_TEST_ITEM_COUNT);
+    BST  *bst = bst_create(bst_test_array, BST_TEST_ITEM_COUNT);
    
     bst_inorder(bst, test_bst_order_process);
 
@@ -145,7 +153,7 @@ static i64 test_bst_preorder(){
     bst_test_array = arr_new(BST_TEST_ITEM_COUNT);
     arr_fill_rand(bst_test_array, BST_TEST_ITEM_COUNT, 898213, SAMPLE_CASE_BEST);
     
-    BinarySearchTree  *bst = bst_create(bst_test_array, BST_TEST_ITEM_COUNT);
+    BST  *bst = bst_create(bst_test_array, BST_TEST_ITEM_COUNT);
    
     bst_preorder(bst, test_bst_order_process);
 
@@ -163,7 +171,7 @@ static i64 test_bst_postorder(){
     bst_test_array = arr_new(BST_TEST_ITEM_COUNT);
     arr_fill_rand(bst_test_array, BST_TEST_ITEM_COUNT, 898213, SAMPLE_CASE_WORST);
     
-    BinarySearchTree  *bst = bst_create(bst_test_array, BST_TEST_ITEM_COUNT);
+    BST  *bst = bst_create(bst_test_array, BST_TEST_ITEM_COUNT);
    
     bst_preorder(bst, test_bst_order_process);
 
@@ -178,7 +186,7 @@ static i64 test_bst_postorder(){
 
 void test_bst(){
     TEST("Binary Search Tree Creation", test_bst_create(BST_TEST_ITEM_COUNT));
-    BinarySearchTree *bst = bst_test_gen;
+    BST *bst = bst_test_gen;
     TEST("Binary Search Tree Searching", bst_search(bst, bst->left->value));
     TEST("Binary Search Tree Insertion", test_bst_insert(bst, random_at_most(BST_TEST_ITEM_COUNT)));
     bst_free(bst);
--- bst.h
+++ bst.h
@@ -6,33 +6,29 @@
 #define BST_TEST_ITEM_COUNT 10000
 #endif
 
-typedef struct BinarySearchTree{
-    i64 value;
-    struct BinarySearchTree *left;
-    struct BinarySearchTree *right;
-} BinarySearchTree;
+typedef struct BST* BinarySearchTree;
 
-BinarySearchTree *bst_create(i64 *arr, siz n);
-void bst_free(BinarySearchTree *bst);
+BinarySearchTree bst_create(i64 *arr, siz n);
+void bst_free(BinarySearchTree bst);
 
-i64 bst_insert(BinarySearchTree *bst, i64 value);
+i64 bst_insert(BinarySearchTree bst, i64 value);
 
-i64 bst_search(BinarySearchTree *bst, i64 value);
+i64 bst_search(BinarySearchTree bst, i64 value);
 
-i64 bst_delete(BinarySearchTree *bst, i64 value);
+i64 bst_delete(BinarySearchTree bst, i64 value);
 
 typedef void(*bst_process)(i64);
 
-void bst_preorder(BinarySearchTree *bst, bst_process process);
-void bst_preorder_nonrec(BinarySearchTree *bst, bst_process process);
+void bst_preorder(BinarySearchTree bst, bst_process process);
+void bst_preorder_nonrec(BinarySearchTree bst, bst_process process);
 
-void bst_inorder(BinarySearchTree *bst, bst_process process);
-void bst_inorder_nonrec(BinarySearchTree *bst, bst_process process);
+void bst_inorder(BinarySearchTree bst, bst_process process);
+void bst_inorder_nonrec(BinarySearchTree bst, bst_process process);
 
-void bst_postorder(BinarySearchTree *bst, bst_process process);
-void bst_preorder_nonrec(BinarySearchTree *bst, bst_process process);
+void bst_postorder(BinarySearchTree bst, bst_process process);
+void bst_preorder_nonrec(BinarySearchTree bst, bst_process process);
 
-i64 bst_count_leaves(BinarySearchTree *bst);
-i64 bst_count_internal(BinarySearchTree *bst);
+i64 bst_count_leaves(BinarySearchTree bst);
+i64 bst_count_internal(BinarySearchTree bst);
 
 void test_bst();
